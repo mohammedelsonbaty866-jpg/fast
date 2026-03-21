3d946eb7695baa61b844e3ccf283900b630dbe6f
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-03-21 16:52:36 +0200

    Initialize SQLite database and create products table

---
+++ server/database.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./pos.db');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      price REAL,
+      quantity INTEGER
+    )
+  `);
+});
+
+module.exports = db;
