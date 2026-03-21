d495502386faaf7eff69554e3c081621f139b453
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-03-21 23:31:14 +0200

    Update server to listen on all interfaces

--- server/server.js
+++ server/server.js
@@ -37,8 +37,8 @@ app.post('/products', (req, res) => {
   res.json(newProduct);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log("Server running on " + PORT);
 });
